4e6b6f17e2127f854615e77885801470b97cdb48
Jurie-Jan Botha <juriejanbotha@gmail.com>
2014-04-15 15:47:42 +0200

    Added selection of default sampler for SRTemplate

--- bika/lims/content/srtemplate.py
+++ bika/lims/content/srtemplate.py
@@ -2,42 +2,58 @@
     Sample Round Template
 """
 
+from Products.Archetypes.public import *
+
 from AccessControl import ClassSecurityInfo
 from bika.lims import bikaMessageFactory as _
 from bika.lims.browser.widgets import RecordsWidget as BikaRecordsWidget
+# from bika.lims.browser.widgets import ReferenceWidget
 from bika.lims.browser.widgets import SRTemplateARTemplatesWidget
 from bika.lims.config import PROJECTNAME
 from bika.lims.content.bikaschema import BikaSchema
 from bika.lims.idserver import renameAfterCreation
-from Products.Archetypes.public import *
+from bika.lims.utils import getUsers
+from Products.Archetypes.references import HoldingReference
 from Products.ATExtensions.field.records import RecordsField
+from Products.CMFCore import permissions
 from Products.CMFCore.utils import getToolByName
+import sys
 
 
 schema = BikaSchema.copy() + Schema((
-    TextField('Instructions',
+    TextField(
+        'Instructions',
         searchable = True,
-        default_content_type = 'text/plain',
-        allowed_content_types= ('text/plain'),
+        default_content_type='text/plain',
+        allowed_content_types=('text/plain'),
         default_output_type="text/plain",
         widget = TextAreaWidget(
             label = _('Instructions'),
             append_only = True,
         ),
     ),
-    ReferenceField('ARTemplates',
-        schemata = 'AR Templates',
-        required = 1,
-        multiValued = 1,
-        allowed_types = ('ARTemplate',),
-        relationship = 'SRTemplateARTemplate',
-        widget = SRTemplateARTemplatesWidget(
-            label = _("AR Templates"),
-            description = _("Select AR Templates to include"),
+    StringField(
+        'Sampler',
+        vocabulary='getSamplers',
+        vocabulary_display_path_bound=sys.maxint,
+        widget=ReferenceWidget(
+            label=_("Default Sampler"),
+            description=_("Select the default Sampler to be assigned"),
+        ),
+    ),
+    ReferenceField(
+        'ARTemplates',
+        schemata='AR Templates',
+        required=1,
+        multiValued=1,
+        allowed_types=('ARTemplate',),
+        relationship='SRTemplateARTemplate',
+        widget=SRTemplateARTemplatesWidget(
+            label=_("AR Templates"),
+            description=_("Select AR Templates to include"),
         )
     ),
-),
-)
+))
 
 
 schema['description'].widget.visible = True
@@ -55,6 +71,10 @@ class SRTemplate(BaseContent):
     _at_rename_after_creation = True
     def _renameAfterCreation(self, check_auto_id=False):
         renameAfterCreation(self)
+
+    def getSamplers(self):
+        samplers = getUsers(self, ['Sampler', 'LabManager', 'Manager'])
+        return samplers
 
 
 registerType(SRTemplate, PROJECTNAME)
